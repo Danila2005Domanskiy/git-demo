7c3cecf7028d8a5ecea363ec43f8ccdfb5c70e8d
Danila Domanskiy <daniladomanskiy@gmail.com>
2024-02-13 21:25:32 +0300

    Modified program

--- gitgui-demo.cpp
+++ gitgui-demo.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello GIT!" << endl;
+    char name[20];
+    cout << "Enter your name: ";
+    cin.getline(name, 20);
+    cout << "Hello GIT! I am " << name << "." << endl;
 }
